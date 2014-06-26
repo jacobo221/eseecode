e7d7dbf3c6753c67de1a1df23f824a0b88578aa2
jacobo221 <jacobo221@gmail.com>
2014-06-26 18:24:14 +0200

    ie9 fails to load and parse script files in order, so use html calls instead

--- js/eseecode.js
+++ js/eseecode.js
@@ -3,62 +3,6 @@
 	 * @author Jacobo Vilella Vilahur
 	 */
 
-	/**
-	 * Loads a style sheet into the DOM
-	 * @private
-	 * @param {!String} url URL of the style sheet to load
-	 * @example loadStyle("css/ui.js")
-	 */
-	function loadStyle(url) {
-		var element = document.createElement("style");
-		element.type = "text/css";
-		element.innerHTML = "@import url(\""+url+"\");";
-		document.getElementsByTagName("head")[0].appendChild(element);
-	}
-
-	/**
-	 * Loads a script into the DOM
-	 * @private
-	 * @param {!String} url URL of the script to load
-	 * @param {function()} callback Function to run once the script is loaded
-	 * @example loadScript("js/common.js")
-	 */
-	function loadScript(url, callback) {
-		var element = document.createElement("script");
-		element.type = "text/javascript";
-		element.src = url;
-		if (callback) {
-			element.onreadystatechange = callback;
-			element.onload = callback;
-		}
-		document.getElementsByTagName("head")[0].appendChild(element);
-	}
-
-	loadStyle("css/common.css");
-	loadStyle("css/ui.css");
-	loadStyle("css/blocks.css");
-	loadScript("js/common.js");
-	loadScript("js/backend.js");
-	loadScript("js/execution.js");
-	loadScript("js/blocks.js");
-	loadScript("js/levelConvert.js");
-	loadScript("js/debug.js");
-	loadScript("js/ui.js");
-	loadScript("js/translate.js");
-	loadScript("js/api.js");
-	loadScript("js/instructions/set.js");
-	loadScript("js/instructions/categories.js");
-	loadScript("js/instructions/implementation.js");
-	loadScript("js/instructions/icons.js");
-	loadScript("js/i18n/ca.js");
-	loadScript("js/i18n/es.js");
-	loadScript("js/jison/eseecodeLanguage.js",function(){
-		loadScript("js/jison/makeBlocks.js");
-		loadScript("js/jison/makeWrite.js");
-	});
-	loadScript("js/ace/ace.js");
-	loadScript("js/jscolor/jscolor.js");
-
 	// Main initialization
 	/**
 	 * @type Array<{platform:{name:{text:String,link:String},version:{text:String,link:String},author:{text:String,link:String},license:{text:String,link:String}},i18n:{available:Array<{*}>,current:String},instructions:{set:Array<{*}>,categories:Array{*},icons:Array{*}},execution:{reakpointCounter:Number,breakpointCounterLimit:Number,step:Number,stepped:Boolean,timeLimit:Number,programCounter:Number,programCounterLimit:Number,endLimit:Number,startTime:Number,sandboxProperties:Array<String>},codeFileName:String,session:{highlight:{lineNumber:Number,reason:String},changesInCode:Boolean,floatingBlock:{div:HTMLElement,fromDiv:HTMLElement},blocksUndo:Array<{*}>,breakpoints:Array<{*}>,breakpointHandler:Boolean|Number,timeoutHandlers:Array<{*}>},whiteboard:HTMLElement,dialogWindow:HTMLElement,canvasArray:Array<{*}>,windowsArray:Array<{*}>,currentCanvas:Object,currentWindow:HTMLElement,setup:{blockWidth:Array<{String, String},blockheight:Array<{String, String}>,defaultFontSize:Number,defaultFontWidth:Number,undoDepth:Number},modes:{console:Array<{*}>,dialog:Array<{*}>}}>
