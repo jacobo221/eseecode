e53c89756a214ac95997525d6c0cd49ce8bb0521
jacobo221 <jacobo221@gmail.com>
2014-07-15 23:21:11 +0200

    detect unsupported browsers

--- js/eseecode.js
+++ js/eseecode.js
@@ -98,6 +98,18 @@
 		}
 	};
 
-	// Init application
-	window.addEventListener("load",function(){resetUI()}, false);
+	// Make sure the browser supports the minimum to run the platform
+	var browserNotSupportedErrorMessage = "Your browser is incompatible with "+$_eseecode.platform.name.text+". It is probably too old!";
+	if (!document || !document.getElementById || !document.createElement || !document.addEventListener || !document.appendChild) {
+		// Can't event support msgBox()
+		alert(browserNotSupportedErrorMessage);
+	} else {
+		var testCanvas = document.createElement('canvas');
+		if (!window.addEventListener || !(testCanvas.getContext && testCanvas.getContext('2d')) || !document.querySelector) {
+			alert(browserNotSupportedErrorMessage+"!!");
+		} else {
+			// Init application
+			window.addEventListener("load",function(){resetUI()}, false);
+		}
+	}
 
