efa2ae0f5d4c5716997d8b8822f2cd1683c2e269
jacobo221 <jacobo221@gmail.com>
2016-04-07 00:42:07 +0000

    style

--- js/ui.js
+++ js/ui.js
@@ -1917,21 +1917,21 @@
 			var element = document.documentElement;
 			if(element.requestFullscreen) {
 				element.requestFullscreen();
-			} else if(element.mozRequestFullScreen) {
+			} else if (element.mozRequestFullScreen) {
 				element.mozRequestFullScreen();
-			} else if(element.webkitRequestFullscreen) {
+			} else if (element.webkitRequestFullscreen) {
 				element.webkitRequestFullscreen();
-			} else if(element.msRequestFullscreen) {
+			} else if (element.msRequestFullscreen) {
 				element.msRequestFullscreen();
 			}
 		} else {
 			if(document.exitFullscreen) {
 				document.exitFullscreen();
-			} else if(document.mozCancelFullScreen) {
+			} else if (document.mozCancelFullScreen) {
 				document.mozCancelFullScreen();
-			} else if(document.webkitExitFullscreen) {
+			} else if (document.webkitExitFullscreen) {
 				document.webkitExitFullscreen();
-			} else if(document.msExitFullscreen) {
+			} else if (document.msExitFullscreen) {
 				document.msExitFullscreen();
 			}
 		}
