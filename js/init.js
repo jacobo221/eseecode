0450d7bccf30aa45d365436dab796459bbc7b3ea
jacobo221 <jvilella@vilahur.com>
2024-01-07 00:18:03 +0100

    refresh on every version bump

--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@
 		platform: {
 			name: "eSeeCode",
 			author: "Jacobo Vilella Vilahur",
+			email: "jvilella@eseecode.com",
 			website: "https://eseecode.com",
 			license: "GPLv3 or later",
 			license_link: "https://gnu.org/licenses/gpl.html",
