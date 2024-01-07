3395f5c069eef5c12ff94afb92ed899b5a5dbcf4
jacobo221 <jvilella@vilahur.com>
2024-01-07 20:32:03 +0100

    docs

--- generate_jsdocs.sh
+++ generate_jsdocs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-dirOut="$HOME/workspace/jsdoc/"
+dirOut="$HOME/eseecode/jsdoc/"
 files="api instructions/implementation"
 cssFile="../jsdoc.css"
 
 mkdir "$dirOut" 2>/dev/null
 jsdocFileOut="$dirOut/symbols/_global_.html"
+tempfilename="$(mktemp).js"
 for file in $files; do
 	echo "Generating $file docs..."
-	jsdoc -d="$dirOut" -q -s "$HOME/workspace/js/$file.js" >/dev/null
+	cat "$HOME/eseecode/js/$file.js" | sed -E 's/^\$e\.api\.([a-zA-Z0-9_]+) = \((.*)\) => \{/function \1(\2) {/' | sed -E 's/^\$e\.api\.([a-zA-Z0-9_]+) = (async )?function /\2function \1/' > "$tempfilename"
+	jsdoc -a -d="$dirOut" -q -s "$tempfilename" >/dev/null
 	fileOut="$dirOut/$(basename $file).html"
 	firstLine="$(grep "====== methods summary =====" "$jsdocFileOut" -n | cut -d: -f1)"
 	lastLine="$(grep "====== event details =====" "$jsdocFileOut" -n | cut -d: -f1)"
@@ -19,4 +21,4 @@ for file in $files; do
 	sed 's/\.\.\/symbols\/_global_\.html//g' "$fileOut".tmp > "$fileOut"
 	rm -rf "$fileOut".tmp
 done
-rm -rf "$dirOut/index.html" "$dirOut/files.html" "$dirOut/symbols/"
+rm -rf "$dirOut/index.html" "$dirOut/files.html" "$dirOut/symbols/" "$tempfilename"
